3b02e67184bffff3c99372949a70cf4eb446c770
Félix Romero José Francisco <51935664+p4n5ho@users.noreply.github.com>
2022-03-31 11:54:25 -0700

    Primera subida
    -Esta casi todo lo gráfico.
    -Es navegable.
    -Esta acomodado visualmente.

---
+++ My project/Assets/Scrips/Control.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Pausar() 
+    { 
+        
+    }
+
+    public void CargarEscena(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+}
