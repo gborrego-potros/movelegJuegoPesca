7ec84764ad15b4eeb1b29a572e75e651108ac5e8
Félix Romero José Francisco <51935664+p4n5ho@users.noreply.github.com>
2022-04-06 22:13:30 -0700

    Cambio a Android y menu de pausa en el juego
    Estaba programando para PC sin darme cuenta, ahora esta en Android.
    Se agrego el menu de pausa en la escena del juego

---
+++ My project/Assets/Scrips/MenuPausa.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+        menuPausa.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void Reiniciar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Cerrar ()
+    {
+        Application.Quit();
+    }
+}
